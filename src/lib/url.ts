ea6ed550026de261ad27e5819464028ed86d6c02
Luis Rodrigues <goblindegook@users.noreply.github.com>
2017-08-09 02:14:19 +0300

    Handle permalinks.

--- src/lib/url.ts
+++ src/lib/url.ts
@@ -1,6 +1,14 @@
-import { sep, parse } from 'path'
+import { format } from 'date-fns'
+import { parse, sep } from 'path'
+import * as S from 'string'
 import { Config } from './config'
 import { Frontmatter } from './frontmatter'
+
+export interface Page extends Frontmatter {
+  date: Date
+  path: string
+  title: string
+}
 
 function slashAtIndex (s: string, index: number): string {
   return s.length > index && s[index] !== '/' ? '/' : ''
@@ -10,17 +18,40 @@ function addSlashes (s: string): string {
   return slashAtIndex(s, 0) + s + slashAtIndex(s, s.length - 1)
 }
 
-function buildUrl (path: string, frontmatter: Frontmatter): string {
-  const { dir, name } = parse(path)
-  const slug = frontmatter.slug || name
+function buildPermalink (page: Page, config: Config): string {
+  const { dir, name } = parse(page.path)
+  const section = dir.split(sep)[0]
 
+  if (section && config.permalinks && config.permalinks[section]) {
+    const title = S(page.title).slugify().s
+
+    return config.permalinks[section]
+      .replace(':yearday', format(page.date, 'DDD'))
+      .replace(':year', format(page.date, 'YYYY'))
+      .replace(':monthname', format(page.date, 'MMMM'))
+      .replace(':month', format(page.date, 'MM'))
+      .replace(':day', format(page.date, 'DD'))
+      .replace(':weekdayname', format(page.date, 'dddd'))
+      .replace(':weekday', format(page.date, 'd'))
+      .replace(':section', section)
+      .replace(':title', title)
+      .replace(':slug', page.slug || title)
+      .replace(':filename', name)
+  } else {
+    return ''
+  }
+}
+
+function buildUrl (page: Page, config: Config): string {
+  const { dir, name } = parse(page.path)
+  const slug = page.slug || name
   return dir.split(sep).concat(slug === '_index' ? [] : slug).join('/')
 }
 
-export function url (path: string, frontmatter: Frontmatter, config: Config): string {
-  if (frontmatter.url) {
-    return frontmatter.url
+export function url (page: Page, config: Config): string {
+  if (page.url) {
+    return page.url
   }
 
-  return addSlashes(buildUrl(path, frontmatter))
+  return addSlashes(buildPermalink(page, config) || buildUrl(page, config))
 }
--- test/lib/url.spec.ts
+++ test/lib/url.spec.ts
@@ -1,33 +1,161 @@
 import { join } from 'path'
-import { url } from '../../src/lib/url'
+import { Page, url } from '../../src/lib/url'
+
+function createPage (overrides = {}): Page {
+  return {
+    date: new Date(),
+    path: join('path', 'to', 'page.md'),
+    ...overrides
+  }
+}
 
 describe('url()', () => {
-  it ('generates a URL from the file path', () => {
-    const path = join('path', 'to', 'content.md')
-    const expected = '/path/to/content/'
+  it('generates a URL from the file path', () => {
+    const page = createPage()
 
-    expect(url(path, {}, {})).toBe(expected)
+    const expected = '/path/to/page/'
+
+    expect(url(page, {})).toBe(expected)
   })
 
-  it ('generates a URL from the file path for _index.md files', () => {
-    const path = join('path', 'to', '_index.md')
+  it('generates a URL from the file path for _index.md files', () => {
+    const page = createPage({ path: join('path', 'to', '_index.md') })
+
     const expected = '/path/to/'
 
-    expect(url(path, {}, {})).toBe(expected)
+    expect(url(page, {})).toBe(expected)
   })
 
-  it ('generates a URL from the file path with a frontmatter "slug" override', () => {
-    const path = join('path', 'to', 'content.md')
-    const frontmatter = { slug: 'slug' }
+  it('generates a URL from the file path with a frontmatter "slug" override', () => {
+    const page = createPage({ slug: 'slug' })
+
     const expected = '/path/to/slug/'
 
-    expect(url(path, frontmatter, {})).toBe(expected)
+    expect(url(page, {})).toBe(expected)
+  })
+
+  it('generates a URL from the permalinks configuration', () => {
+    const page = createPage()
+    const config = { permalinks: { path: 'test' } }
+
+    const expected = '/test/'
+
+    expect(url(page, config)).toBe(expected)
+  })
+
+  it('generates a URL from the permalinks configuration, replacing :year from page date', () => {
+    const page = createPage({ date: new Date(2017, 7) })
+    const config = { permalinks: { path: '/:year/' } }
+
+    const expected = '/2017/'
+
+    expect(url(page, config)).toBe(expected)
+  })
+
+  it('generates a URL from the permalinks configuration, replacing :month from page date', () => {
+    const page = createPage({ date: new Date(2017, 7) })
+    const config = { permalinks: { path: '/:month/' } }
+
+    const expected = '/08/'
+
+    expect(url(page, config)).toBe(expected)
+  })
+
+  it('generates a URL from the permalinks configuration, replacing :monthname from page date', () => {
+    const page = createPage({ date: new Date(2017, 7) })
+    const config = { permalinks: { path: '/:monthname/' } }
+
+    const expected = '/August/'
+
+    expect(url(page, config)).toBe(expected)
+  })
+
+  it('generates a URL from the permalinks configuration, replacing :day from page date', () => {
+    const page = createPage({ date: new Date(2017, 7, 9) })
+    const config = { permalinks: { path: '/:day/' } }
+
+    const expected = '/09/'
+
+    expect(url(page, config)).toBe(expected)
+  })
+
+  it('generates a URL from the permalinks configuration, replacing :weekday from page date', () => {
+    const page = createPage({ date: new Date(2017, 7, 9) })
+    const config = { permalinks: { path: '/:weekday/' } }
+
+    const expected = '/3/'
+
+    expect(url(page, config)).toBe(expected)
+  })
+
+  it('generates a URL from the permalinks configuration, replacing :weekday from page date', () => {
+    const page = createPage({ date: new Date(2017, 7, 9) })
+    const config = { permalinks: { path: '/:weekdayname/' } }
+
+    const expected = '/Wednesday/'
+
+    expect(url(page, config)).toBe(expected)
+  })
+
+  it('generates a URL from the permalinks configuration, replacing :yearday from page date', () => {
+    const page = createPage({ date: new Date(2017, 7, 9) })
+    const config = { permalinks: { path: '/:yearday/' } }
+
+    const expected = '/221/'
+
+    expect(url(page, config)).toBe(expected)
+  })
+
+  it('generates a URL from the permalinks configuration, replacing :section from page section', () => {
+    const page = createPage({})
+    const config = { permalinks: { path: '/:section/' } }
+
+    const expected = '/path/'
+
+    expect(url(page, config)).toBe(expected)
+  })
+
+  it('generates a URL from the permalinks configuration, replacing :slug from page slug', () => {
+    const page = createPage({ slug: 'slug' })
+    const config = { permalinks: { path: '/:slug/' } }
+
+    const expected = '/slug/'
+
+    expect(url(page, config)).toBe(expected)
+  })
+
+  it('generates a URL from the permalinks configuration, replacing :title from page title', () => {
+    const page = createPage({ title: 'Page Title' })
+    const config = { permalinks: { path: '/:title/' } }
+
+    const expected = '/page-title/'
+
+    expect(url(page, config)).toBe(expected)
+  })
+
+  it('generates a URL from the permalinks configuration, replacing :slug from page title if slug undefined', () => {
+    const page = createPage({ title: 'Page Title' })
+    const config = { permalinks: { path: '/:slug/' } }
+
+    const expected = '/page-title/'
+
+    expect(url(page, config)).toBe(expected)
+  })
+
+  it('generates a URL from the permalinks configuration, replacing :filename from page filename', () => {
+    const page = createPage()
+    const config = { permalinks: { path: '/:filename/' } }
+
+    const expected = '/page/'
+
+    expect(url(page, config)).toBe(expected)
   })
 
   it('generates a URL taken from the frontmatter "url" field', () => {
-    const frontmatter = { url: 'test-url' }
-    const expected = frontmatter.url
+    const page = createPage({ url: 'test-url' })
 
-    expect(url('', frontmatter, {})).toBe(expected)
+    const expected = page.url
+
+    expect(url(page, {})).toBe(expected)
   })
 })
