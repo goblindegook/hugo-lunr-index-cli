948e074749dc893ef8b68de575a27cabc6c1e52d
Luis Rodrigues <goblindegook@users.noreply.github.com>
2017-08-10 15:18:56 +0300

    Configuration parser

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,12 @@
+import toml from 'toml'
+
 export interface Config {
   [key: string]: any
   permalinks?: {
     [section: string]: string
   }
+}
+
+export function parse (config: string): Config {
+  return toml.parse(config)
 }
---
+++ test/lib/config.spec.ts
@@ -0,0 +1,19 @@
+import { parse } from '../../src/lib/config'
+
+test('Parse empty config', () => {
+  expect(parse('')).toEqual({})
+})
+
+test('Parse config', () => {
+  const toml = `array = [ "toml", "config", "test" ]
+date = 2017-08-08T00:00:00+03:00
+string = "TOML Config"`
+
+  const expected = {
+    array: ['toml', 'config', 'test'],
+    date: new Date('2017-08-08T00:00:00+03:00'),
+    string: 'TOML Config'
+  }
+
+  expect(parse(toml)).toEqual(expected)
+})
